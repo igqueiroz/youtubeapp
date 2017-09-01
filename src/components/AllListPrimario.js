0392d4a2859fc927478e4b4b77f9bf7ec4dab1cb
Igor Queiroz <andyarm@gmail.com>
2017-08-31 21:17:27 -0300

    Add files via upload

--- src/components/AllListPrimario.js
+++ src/components/AllListPrimario.js
@@ -1,49 +1,21 @@
 import React, {Component}  from 'react'
 import ReactDOM from 'react-dom'
 import Ellipsis from 'FTEllipsis'
-
+import ConvertNumbers from './ConvertNumbers'
+import ChannelList from '../services/ChannelList'
 
 export default class AllListPrimario extends Component {	
 	componentDidMount() {
-			function format(num){
-			    var n = num.toString(), p = n.indexOf('.');
-			    return n.replace(/\d(?=(?:\d{3})+(?:\.|$))/g, function($0, i){
-			        return p<0 || i<p ? ($0+'.') : $0;
-			    });
-			}
-	    	var ytkey = 'AIzaSyB6TDiesYuXK36ogjLqcC5myC6MKACe9Uo';
-            youtubeApiCall();
+        // ConvertNumbers.NumberFormat
+        // ConvertNumbers.NumberConvertDate
+        // ConvertNumbers.NumberDurationToSeconds
             $("#load-more-videos").on( "click", function( event ) {
-                $('.load').show();
+                $('.load').fadeIn();
                 $("#load-more-videos").hide();
-                $("#pageToken").val($("#load-more-videos").val());
-                youtubeApiCall();
+                handleSubmit();
             });
-			
-            function getVideoDetails(ids){
-                $.ajax({
-                    cache: false,
-                    data: $.extend({
-                        key: ytkey,
-                        part: 'snippet,contentDetails,statistics'
-                    }, {id: ids}),
-                    dataType: 'json',
-                    type: 'GET',
-                    timeout: 5000,
-                    fields: "items(id,contentDetails,statistics(viewCount),snippet(publishedAt,channelTitle,channelId,title,description,views,thumbnails(high)))",
-                    url: 'https://www.googleapis.com/youtube/v3/videos'
-                })
-                .done(function(data) {
-                    var items = data.items, videoList = "";
-                    var destaqueFetch = data.items[0];
-                    $.each(items, function(index,e) {
-                        videoList = videoList + '<li class="videos" data-desc="'+ e.snippet.description +'" data-toggle="modal" data-target="#myModal" data-theVideo="'+e.id+'" data-title="'+e.snippet.title+'" data-views="'+format(e.statistics.viewCount)+'" data-publication="'+YTConvertDate(e.snippet.publishedAt)+'"><div class="video-img"><img src="'+e.snippet.thumbnails.default.url+'"/><span>'+YTDurationToSeconds(e.contentDetails.duration)+'</span></div><div class="ellipsis"><h3 class="video-title">'+e.snippet.title+'</h3></div><span class="video-icon-views">'+format(e.statistics.viewCount)+' views</span></li>'
-                    });
-                    $("#video-nav").append(videoList);
-                    tools();
-                });
-            }
-            tools();
+            handleSubmit()
+            
             function tools() {
 				var forEach = Array.prototype.forEach;
 				var els = document.getElementsByClassName('ellipsis');
@@ -54,94 +26,70 @@ export default class AllListPrimario extends Component {
 				});
 
             }
-            
-            function YTDurationToSeconds(duration) {
-                var match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/)
-
-                var hours = ((parseInt(match[1]) || 0) !== 0)?parseInt(match[1])+":":"";
-                var minutes = ((parseInt(match[2]) || 0) !== 0)?parseInt(match[2])+":":"0";
-                var seconds = ((parseInt(match[3]) || 0) !== 0)?parseInt(match[3]):"00";
-                var total = hours + minutes + seconds;
-                return total;
-            }
-
-            function YTConvertDate(dates) {
-                var date =  dates.split('T');
-                date = date[0].split('-');
-                if (date[1] == "01") {date[1] =  "Janeiro"};
-                if (date[1] == "02") {date[1] =   "Fevereiro"};
-                if (date[1] == "03") {date[1] =   "Março"};
-                if (date[1] == "04") {date[1] =   "Abril"};
-                if (date[1] == "05") {date[1] =   "Maio"};
-                if (date[1] == "06") {date[1] =   "Junho"};
-                if (date[1] == "07") {date[1] =   "Julho"};
-                if (date[1] == "08") {date[1] =   "Agosto"}; 
-                if (date[1] == "09") {date[1] =   "Setembro"}; 
-                if (date[1] == "10") {date[1] =   "Outubro"};
-                if (date[1] == "11") {date[1] =   "Novembro"};
-                if (date[1] == "12") {date[1] =   "Dezembro"};
-                return date[2] + ' de ' + date[1] + ' de ' + date[0];
-            }
-            
-            function youtubeApiCall(){
-                $.ajax({
-                    cache: false,
-                    data: $.extend({
-                        key: ytkey,
-                        part: 'snippet',
-                        channelId: 'UCO9XI15xOtOyEWOYpn0whDA'
-                    }, 
-                    {maxResults:12,pageToken:$("#pageToken").val()}),
-                    dataType: 'json',
-                    type: 'GET',
-                    timeout: 5000,
-                    fields: "pageInfo,items(id(videoId)),nextPageToken",
-                    url: 'https://www.googleapis.com/youtube/v3/search'
-                })
-                .done(function(data) {
-                    if (typeof data.nextPageToken === "undefined") {
-                        $("#load-more-videos").hide();;
-                    }
-                    else{$("#load-more-videos").show();
-                    }
-                    var items = data.items, videoids = [];
-                    $("#load-more-videos").val(data.nextPageToken);
-                    $.each(items, function(index,e) {
-                        videoids.push(e.id.videoId);
-                    });
-                    getVideoDetails(videoids.join());
-                    $('.load').hide();
+            function handleSubmit() {
+                var pageTokenInput = document.querySelector('#pageToken').value;
+                var qtidadeMaxIds = 12;
+                ChannelList.listarCanal(qtidadeMaxIds,pageTokenInput).then((response) => {
+                    var videosId = response.data.items, videosIds = [], nextPageTokenId = response.data.nextPageToken
+                    videosId.forEach(e => {
+                        videosIds.push(e.id.videoId);
+                    })
+                    $("#pageToken").val(nextPageTokenId);
                     
-					$('#myModal').on('show.bs.modal', function (event) {
-						var button = $(event.relatedTarget);
-						var title = button.data('title');
-						var views = button.data('views');
-						var publication = button.data('publication');
-						var description = button.data('desc');
-						var text = button.data('text');
-						var thevideo = button.data('thevideo')
-						var text = button.data('text');
-						var modal = $(this);
-						modal.find('h4').text(title);
-						$('.tips').remove();
-						$('#video-info h4').after('<span data-toggle="tooltip" data-placement="top" title="'+ views + ' views" class="tips video-info-views" /><span data-toggle="tooltip" data-placement="top" title="'+ publication + '" class="tips video-info-date" />');
-						
-						modal.find('.video-info-text-desc').text(description);
-						$('#youtube-destaque iframe').attr('src','https://www.youtube.com/embed/' + thevideo + '?autoplay=1');
+                    ChannelList.listarIds(videosIds.join()).then((response) => {
+                        var items = response.data.items, videoList = "";
+                        var destaqueFetch = response.data.items[0];
+                        
+                        response.data.items.forEach(e => {
+                        videoList = videoList + '<li data-desc="'+ e.snippet.description +'" data-url="'+ e.id +'" class="videos"  title="' + ConvertNumbers.NumberConvertDate(e.snippet.publishedAt)+'"><div class="video-img"><img src="'+ e.snippet.thumbnails.high.url+'" alt="'+e.snippet.title+'"><span class="durtime">'+ ConvertNumbers.NumberDurationToSeconds(e.contentDetails.duration)+'</span></div><span class="video-title">'+e.snippet.title+'</span><span class="video-icon-views">'+ConvertNumbers.NumberFormat(e.statistics.viewCount)+' views</span></li>';
+                        });
+                        $("#video-nav").append(videoList);
+                        
+                        $('[data-toggle="tooltip"]').tooltip({ placement:"auto"})
+                        loadVideoContainer();
+                        if (typeof nextPageTokenId === "undefined") {
+                            $("#load-more-videos").hide();
+                            $('.load').hide();
+                        }
+                        else {
+                            $('.load').hide();
+                            $("#load-more-videos").fadeIn();
+                        }
+                    })
+                })
+                tools();
+                loadVideoContainer()
+            }
+            function loadVideoContainer() {
+            $('#myModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget);
+                var title = button.data('title');
+                var views = button.data('views');
+                var publication = button.data('publication');
+                var description = button.data('desc');
+                var text = button.data('text');
+                var thevideo = button.data('thevideo')
+                var text = button.data('text');
+                var modal = $(this);
+                modal.find('h4').text(title);
+                $('.tips').remove();
+                $('#video-info h4').after('<span data-toggle="tooltip" data-placement="top" title="'+ views + ' views" class="tips video-info-views" /><span data-toggle="tooltip" data-placement="top" title="'+ publication + '" class="tips video-info-date" />');
+                
+                modal.find('.video-info-text-desc').text(description);
+                $('#youtube-destaque iframe').attr('src','https://www.youtube.com/embed/' + thevideo + '?autoplay=1');
 
-						modal.find('.close').click(function () {
-							$('#youtube-destaque iframe').attr('src', '');
-						});
-						$(document).click(function (e) {
-							if (e.target === $('#myModal')[0] && $('body').hasClass('modal-open')) {
-								$('#youtube-destaque iframe').attr('src', '');
-							}
-						})
-						$('[data-toggle="tooltip"]').tooltip({ placement:"auto"})
-               		 });
-            });			
-		}
-	}
+                modal.find('.close').click(function () {
+                    $('#youtube-destaque iframe').attr('src', '');
+                });
+                $(document).click(function (e) {
+                    if (e.target === $('#myModal')[0] && $('body').hasClass('modal-open')) {
+                        $('#youtube-destaque iframe').attr('src', '');
+                    }
+                })
+                    $('[data-toggle="tooltip"]').tooltip({ placement:"auto"})
+                });
+            }
+    }
 	render() {
 		return(
 			<div>
